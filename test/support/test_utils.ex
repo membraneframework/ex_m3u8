83d185534d95c6bb514e79128dbc21ee2d134adf
feliks.pobiedzinski@swmansion.com <feliks.pobiedzinski@swmansion.com>
2025-06-30 16:40:30 +0200

    Fix typo

--- test/support/test_utils.ex
+++ test/support/test_utils.ex
@@ -1,7 +1,7 @@
 defmodule ExM3U8.TestUtils do
   @moduledoc false
 
-  @spec serialize(any()) :: String.t(0)
+  @spec serialize(any()) :: String.t()
   def serialize(data) do
     data
     |> ExM3U8.Serializer.serialize()
